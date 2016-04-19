3cc0694d8fd38ed6e8bc7fd13b03972926ea3054
stanislav <stas.yashchenko@themidnightcoders.com>
2016-04-19 14:29:42 +0300

    v3.1.6

--- libs/backendless.js
+++ libs/backendless.js
@@ -1,4 +1,4 @@
-// Backendless.js 3.1.5
+// Backendless.js 3.1.6
 
 (function(factory) {
     var root = (typeof self == 'object' && self.self === self && self) ||
@@ -36,7 +36,7 @@
         emptyFn     = (function() {
         });
 
-    Backendless.VERSION = '3.1.5';
+    Backendless.VERSION = '3.1.6';
     Backendless.serverURL = 'https://api.backendless.com';
 
     Backendless.noConflict = function() {
