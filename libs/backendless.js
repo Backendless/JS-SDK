6048637c9595e943950ba22423230d89f16a5112
ilya.veklenko <ilya.veklenko@themidnightcoders.com>
2016-06-13 16:04:53 +0300

    v3.1.11

--- libs/backendless.js
+++ libs/backendless.js
@@ -1,4 +1,4 @@
-// Backendless.js 3.1.10
+// Backendless.js 3.1.11
 
 (function(factory) {
     var root = (typeof self == 'object' && self.self === self && self) ||
@@ -36,7 +36,7 @@
         emptyFn     = (function() {
         });
 
-    Backendless.VERSION = '3.1.10';
+    Backendless.VERSION = '3.1.11';
     Backendless.serverURL = 'https://api.backendless.com';
 
     Backendless.noConflict = function() {
