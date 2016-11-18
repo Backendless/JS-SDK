6690a14fb2521f8c14b885077987eb9f21952a7e
stanislav <stas.yashchenko@themidnightcoders.com>
2016-11-18 17:05:00 +0200

    fix geo service example due to 4.0 sdk version

--- examples/geo-service/citysearch/js/citysearch.js
+++ examples/geo-service/citysearch/js/citysearch.js
@@ -1,65 +1,61 @@
-var APP_ID = '',
+var APP_ID     = '',
     SECRET_KEY = '',
     CATEGORY = 'geoservice_sample',
     FENCE_NAME = '';
 
 function initApp() {
-    Backendless.initApp(APP_ID, SECRET_KEY);
+  Backendless.initApp(APP_ID, SECRET_KEY);
 }
 
-if (!APP_ID || !SECRET_KEY)
-    alert("Missing application ID and secret key arguments. Login to Backendless Console, select your app and get the ID and key from the Manage > App Settings screen. Copy/paste the values into the Backendless.initApp call located in citysearch.js");
+if (!APP_ID || !SECRET_KEY) {
+  alert(
+    "Missing application ID or secret key arguments. Login to Backendless Console, select your app and get the ID and key from the Manage > App Settings screen. Copy/paste the values into the Backendless.initApp call located in citysearch.js");
+}
 
 function FenceEvent(args) {
-    args = args || {};
-    this.latitude = args.latitude || 0;
-    this.longitude = args.longitude || 0;
-    this.fenceName = args.fenceName || "defaultName";
-    this.rule = args.rule || "noname";
+  args = args || {};
+  this.latitude = args.latitude || 0;
+  this.longitude = args.longitude || 0;
+  this.fenceName = args.fenceName || "defaultName";
+  this.rule = args.rule || "noname";
 }
 
 var myLocation = new GeoPoint();
 
 var inAppCallBack = {
-    onenter: function (geofenceName, geofenceId, latitude, longitude) {
-        var logger = new FenceEvent({
-            latitude: latitude,
-            longitude: longitude,
-            fenceName: geofenceName,
-            rule: "onEnter"
-        });
-        Backendless.Persistence.of(FenceEvent).save(logger);
-    },
+  onenter: function(geofenceName, geofenceId, latitude, longitude) {
+    var logger = new FenceEvent({
+      latitude : latitude,
+      longitude: longitude,
+      fenceName: geofenceName,
+      rule     : "onEnter"
+    });
 
-    onstay: function (geofenceName, geofenceId, latitude, longitude) {
-        var logger = new FenceEvent({
-            latitude: latitude,
-            longitude: longitude,
-            fenceName: geofenceName,
-            rule: "onStay"
-        });
-        Backendless.Persistence.of(FenceEvent).save(logger);
-    },
+    Backendless.Persistence.of(FenceEvent).save(logger);
+  },
 
-    onexit: function (geofenceName, geofenceId, latitude, longitude) {
-        var logger = new FenceEvent({
-            latitude: latitude,
-            longitude: longitude,
-            fenceName: geofenceName,
-            rule: "onExit"
-        });
-        Backendless.Persistence.of(FenceEvent).save(logger);
-    }
+  onstay: function(geofenceName, geofenceId, latitude, longitude) {
+    var logger = new FenceEvent({
+      latitude : latitude,
+      longitude: longitude,
+      fenceName: geofenceName,
+      rule     : "onStay"
+    });
+
+    Backendless.Persistence.of(FenceEvent).save(logger);
+  },
+
+  onexit: function(geofenceName, geofenceId, latitude, longitude) {
+    var logger = new FenceEvent({
+      latitude : latitude,
+      longitude: longitude,
+      fenceName: geofenceName,
+      rule     : "onExit"
+    });
+
+    Backendless.Persistence.of(FenceEvent).save(logger);
+  }
 };
-
-var completionCallback = new Backendless.Async(
-    function (result) {
-        console.log("geofence monitoring has been started");
-    },
-    function (result) {
-        console.log("error - " + result.message);
-    }
-);
 
 initApp();
 
@@ -69,12 +65,20 @@ Backendless.Geo.startGeofenceMonitoringWithInAppCallback(null, inAppCallBack);
 // start monitoring fence with name FENCE_NAME with callback defined on client
 //Backendless.Geo.startGeofenceMonitoringWithInAppCallback(FENCE_NAME, inAppCallBack);
 
+//var onStartMonitoringSuccess = function(result) {
+//  console.log("geofence monitoring has been started");
+//};
+
+//var onError = function(error) {
+//  console.log(error);
+//};
+
 // start monitoring all fences with callback defined on server
-//Backendless.Geo.startGeofenceMonitoringWithRemoteCallback(null, myLocation, completionCallback);
+//Backendless.Geo.startGeofenceMonitoringWithRemoteCallback(null, myLocation).then(onStartMonitoringSuccess, onError);
 
 // start monitoring fence with name FENCE_NAME with callback defined on server
-//Backendless.Geo.startGeofenceMonitoringWithRemoteCallback(FENCE_NAME, myLocation, completionCallback);
-
+//Backendless.Geo.startGeofenceMonitoringWithRemoteCallback(FENCE_NAME, myLocation, completionCallback)
+//  .then(onStartMonitoringSuccess, onError);
 
 var $resultBlock;
 var $count;
@@ -82,91 +86,92 @@ var $thead;
 var $tbody;
 var $geoQuery;
 
-$(function () {
-    $('#radius-slider').slider({
-        range: "min",
-        value: 1,
-        min: 1,
-        max: 20000,
-        slide: function (event, ui) {
-            $("#radius-value").text(ui.value);
-        }
-    });
+$(function() {
+  $('#radius-slider').slider({
+    range: "min",
+    value: 1,
+    min  : 1,
+    max  : 20000,
+    slide: function(event, ui) {
+      $("#radius-value").text(ui.value);
+    }
+  });
 
-    $("#search-button").click(function () {
-        searchGeoPoints();
-    });
+  $("#search-button").click(function() {
+    searchGeoPoints();
+  });
 
-    $resultBlock = $('#result-block');
-    $count = $('#total-count');
-    $thead = $('#thead');
-    $tbody = $('#tbody');
+  $resultBlock = $('#result-block');
+  $count = $('#total-count');
+  $thead = $('#thead');
+  $tbody = $('#tbody');
 
-    $geoQuery = new BackendlessGeoQuery();
-    $geoQuery.latitude = parseInt($('#latitude').text());
-    $geoQuery.longitude = parseInt($('#longitude').text());
-    $geoQuery.units = Backendless.Geo.UNITS.KILOMETERS;
-    $geoQuery.categories.push(CATEGORY);
+  $geoQuery = new BackendlessGeoQuery();
+  $geoQuery.latitude = parseInt($('#latitude').text());
+  $geoQuery.longitude = parseInt($('#longitude').text());
+  $geoQuery.units = Backendless.Geo.UNITS.KILOMETERS;
+  $geoQuery.categories.push(CATEGORY);
 
-    if (navigator.geolocation)
-        navigator.geolocation.getCurrentPosition(applyLocation);
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(applyLocation);
+  }
 });
 
 function applyLocation(position) {
-    $('#latitude').text(position.coords.latitude);
-    $geoQuery.latitude = position.coords.latitude;
+  $('#latitude').text(position.coords.latitude);
+  $geoQuery.latitude = position.coords.latitude;
 
-    $('#longitude').text(position.coords.longitude);
-    $geoQuery.longitude = position.coords.longitude;
+  $('#longitude').text(position.coords.longitude);
+  $geoQuery.longitude = position.coords.longitude;
 }
 
 function searchGeoPoints() {
-    startLoading();
+  startLoading();
 
-    $tbody.empty();
-    $count.text('loading...');
-    $resultBlock.hide();
+  $tbody.empty();
+  $count.text('loading...');
+  $resultBlock.hide();
 
-    $geoQuery.radius = $('#radius-value').text();
-    Backendless.Geo.find($geoQuery, new Backendless.Async(onResult, onFault, this));
-    Backendless.Geo.getGeopointCount($geoQuery, new Backendless.Async(onCountResult, onFault, this));
+  $geoQuery.radius = $('#radius-value').text();
+  Backendless.Geo.find($geoQuery).then(onResult, onFault);
+  Backendless.Geo.getGeopointCount($geoQuery).then(onCountResult, onFault);
 }
 
 function onCountResult(count) {
-    $count.text(count);
+  $count.text(count);
 }
 
 function onResult(result) {
-    finishLoading();
-    $resultBlock.show();
+  finishLoading();
+  $resultBlock.show();
 
-    if (!result.length) {
-        $thead.hide();
-        $tbody.append("<h3 style='text-align: center'>No geo points found</h3>");
-        return;
-    }
+  if (!result.length) {
+    $thead.hide();
+    $tbody.append("<h3 style='text-align: center'>No geo points found</h3>");
+    return;
+  }
 
-    $.each(result, function () {
-        var cells = [
-            "<td>" + this.metadata.city + "</td>", "<td>" + this.latitude + "</td>", "<td>" + this.longitude + "</td>"
-        ];
+  $.each(result, function() {
+    var cells = [
+      "<td>" + this.metadata.city + "</td>", "<td>" + this.latitude + "</td>", "<td>" + this.longitude + "</td>"
+    ];
 
-        $thead.show();
-        $tbody.append("<tr>" + cells.join('') + "</tr>");
-    });
+    $thead.show();
+    $tbody.append("<tr>" + cells.join('') + "</tr>");
+  });
 }
 
 function onFault(fault) {
-    finishLoading();
-    alert(fault.message);
+  finishLoading();
+  alert(fault.message);
 }
 
 function startLoading() {
-    $('#search-button').hide();
-    $('#loader').show();
+  $('#search-button').hide();
+  $('#loader').show();
 }
 
 function finishLoading() {
-    $('#search-button').show();
-    $('#loader').hide();
+  $('#search-button').show();
+  $('#loader').hide();
 }
