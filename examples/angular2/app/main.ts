0048183eb8dd8d7b55838f734ec7c838971691fa
stanislav <stas.yashchenko@themidnightcoders.com>
2016-11-21 11:34:39 +0200

    fix angular2-beta7 example

--- examples/angular2/app/main.ts
+++ examples/angular2/app/main.ts
@@ -1,27 +1,30 @@
-import { bootstrap}    from 'angular2/platform/browser'
+import { bootstrap } from 'angular2/platform/browser'
 import { AppComponent } from './app.component'
 import { Comment } from './comment'
 import Backendless from 'backendless';
 
 var APP_ID:string = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
 var APP_KEY:string = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
-var APP_VER:string = 'v1';
 
-Backendless.initApp(APP_ID, APP_KEY, APP_VER);
+Backendless.initApp(APP_ID, APP_KEY);
 
 var user:Backendless.User = new Backendless.User();
 user.email = 'michael@backendless.com';
 user.password = "my_super_password";
 
-try {
-    Backendless.UserService.register(user);
-} catch (error) {
+var onError = function(error) {
     console.log(error);
-}
+};
 
-var dataStore:Backendless.DataStore = Backendless.Persistence.of(Comment);
-var commentObject:Comment = new Comment({message: "I'm in", authorEmail: user.username});
+var saveComment = function() {
+    var dataStore:Backendless.DataStore = Backendless.Persistence.of(Comment);
+    var commentObject:Comment = new Comment({message: "I'm in", authorEmail: user.username});
 
-dataStore.save(commentObject);
+    dataStore.save(commentObject).then(function(result) {
+        console.log(result);
+    }, console.error);
+};
+
+Backendless.UserService.register(user).then(saveComment, onError);
 
 bootstrap(<Function>AppComponent);
