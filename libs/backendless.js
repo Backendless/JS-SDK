a1e77f0a826de99a3f9a4ee76f60887c7a31d8ae
stanislav <stas.yashchenko@themidnightcoders.com>
2016-11-22 16:25:05 +0200

    fix error messages

--- libs/backendless.js
+++ libs/backendless.js
@@ -564,7 +564,7 @@
         config.isAsync = (typeof config.isAsync == 'boolean') ? config.isAsync : false;
 
         if (!config.isAsync) {
-            throw new Error('Use Async type of request using Backendless with NodeJS. Add Backendless.Async(successCallback, errorCallback) as last argument');
+            throw new Error('Use Async type of request using Backendless with NodeJS. Add Async(successCallback, errorCallback) as last argument');
         }
 
         if (typeof config.data !== "string") {
@@ -1135,7 +1135,7 @@
             if (!(args[0] instanceof Backendless.DataQueryBuilder) && !(args[0] instanceof Async)) {
                 throw new Error(
                     'Invalid find method argument. ' +
-                    'The argument should be instance of Backendless.DataQueryBuilder or Backendless.Async'
+                    'The argument should be instance of Backendless.DataQueryBuilder or Async'
                 );
             }
         },
