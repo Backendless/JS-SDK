0b0bc7c013634f3aa07d989db5bcbb41e3dca699
grin <stanislaw.grin@gmail.com>
2016-12-27 18:38:18 +0200

    persistence tests

--- test/e2e/specs/persistence.js
+++ test/e2e/specs/persistence.js
@@ -7,7 +7,33 @@ function Foo() {
 
 describe('Backendless.Persistence', function() {
 
-  sandbox.forSuite({})
+  sandbox.forSuite({
+    app: {
+      data: {
+        Users    : [
+          {
+            email   : 'john@lennon.co',
+            name    : 'John Lennon',
+            password: 'beatlesforever'
+          }, {
+            email   : 'paul@mccartney.co',
+            name    : 'Paul Mccartney',
+            password: 'beatlesforever'
+          }, {
+            email   : 'george@harrison.co',
+            name    : 'George Harrison',
+            password: 'beatlesforever'
+          }
+        ],
+        Blackstar: [
+          {
+            numericCol: 1,
+            boolCol: false
+          }
+        ]
+      }
+    }
+  })
 
   it('Create new table', function() {
     const entity = new Foo();
@@ -23,11 +49,11 @@ describe('Backendless.Persistence', function() {
   });
 
   it('Find Record By objectId', function() {
-    var entity = new Foo()
+    let entity = new Foo()
     entity.firstName = 'Bill'
     entity.lastName = 'Gates'
 
-    var db = Backendless.Persistence.of(Foo)
+    const db = Backendless.Persistence.of(Foo)
 
     return db.save(entity)
       .then(result => entity = result)
@@ -72,6 +98,63 @@ describe('Backendless.Persistence', function() {
         expect(result.length).to.be.equal(2)
         expect(result[0].objectId).not.to.be.equal(toRemove.objectId)
         expect(result[1].objectId).not.to.be.equal(toRemove.objectId)
+      })
+  })
+
+  it('Add record to Users table', function() {
+    const db = Backendless.Persistence.of(Backendless.User)
+
+    const user = {
+      email   : 'ringo@starr.co',
+      name    : 'Ringo Starr',
+      password: 'beatlesforever'
+    }
+
+    return Promise.resolve()
+      .then(() => db.save(user))
+      .then(result => {
+        expect(result).to.be.an.instanceof(Backendless.User)
+        expect(result).to.have.property('email').that.equal(user.email)
+        expect(result).to.have.property('name').that.equal(user.name)
+        expect(result).to.not.have.property('password')
+      })
+  })
+
+  it('Impossible to get persistence of Users using string signature', function() {
+    const db = () => Backendless.Persistence.of('Users')
+
+    const expectedError = "Table 'Users' is not accessible through this signature. " +
+      "Use Backendless.Data.of( Backendless.User ) instead"
+
+    expect(db).to.throw(expectedError)
+  })
+
+  it('Check instance of objects from Users table', function() {
+    const db = Backendless.Persistence.of(Backendless.User)
+
+    return Promise.resolve()
+      .then(() => db.find())
+      .then(result => {
+        result.forEach(object => expect(result).to.be.an.instanceof(Backendless.User))
+      })
+  })
+
+  it('Update table record with invalid data type for properties', function() {
+    const db = Backendless.Persistence.of('Blackstar')
+    const expectedError = 'Unable to save object - invalid data type for properties - numericCol, boolCol. ' +
+      'You can change the property type in developer console.'
+
+    return Promise.resolve()
+      .then(() => db.findFirst())
+      .then(result => {
+        result.numericCol = 'String value'
+        result.boolCol = 'String value'
+
+        return result
+      })
+      .then(result => db.save(result))
+      .catch(error => {
+        expect(error.message).to.be.equal(expectedError)
       })
   })
 })
