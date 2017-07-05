651ba006cab7e32923b898dd14fb13ca0182564d
grin <stanislaw.grin@gmail.com>
2017-07-05 14:33:16 +0300

    fix tests

--- test/e2e/specs/events.js
+++ test/e2e/specs/events.js
@@ -7,15 +7,15 @@ describe('Backendless.Events', function() {
   sandbox.forSuite()
 
   it('dispatch event with event arguments', function() {
-    expect(Backendless.Events.dispatch('customEvent', {})).to.eventually.be.empty
+    return expect(Backendless.Events.dispatch('customEvent', {})).to.eventually.be.empty
   })
 
   it('dispatch event without event arguments', function() {
-    expect(Backendless.Events.dispatch('customEvent')).to.eventually.be.empty
+    return expect(Backendless.Events.dispatch('customEvent')).to.eventually.be.empty
   })
 
   it('dispatch event without event name', function() {
-    expect(Backendless.Events.dispatch()).to.eventually.be
+    return expect(Backendless.Events.dispatch()).to.eventually.be
       .rejectedWith(Error, 'Event Name must be provided and must be not an empty STRING!')
   })
 
