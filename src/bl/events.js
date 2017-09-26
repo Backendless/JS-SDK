1dda5cdb12257f67884ad8edd06b086af745d79d
Vladmir Upirov <vladimir.upirov@themidnightcoders.com>
2017-09-26 11:03:33 +0200

    refactor: (Bl) CustomServices/Event
    - move function of CustomServices/Event to separated files

--- src/bl/events.js
+++ src/bl/events.js
@@ -1,37 +1,12 @@
-import Backendless from '../bundle'
 import Utils from '../utils'
-import Async from '../request/async'
-import Urls from '../urls'
 
-const dispatch = (eventName, eventArgs) => {
-  if (!eventName || !Utils.isString(eventName)) {
-    throw new Error('Event Name must be provided and must be not an empty STRING!')
-  }
-
-  eventArgs = (Utils.isObject(eventArgs) && !(eventArgs instanceof Async)) ? eventArgs : {}
-
-  let responder = Utils.extractResponder(arguments)
-
-  if (responder) {
-    responder = Utils.wrapAsync(responder)
-  }
-
-  eventArgs = eventArgs instanceof Async ? {} : eventArgs
-
-  return Backendless._ajax({
-    method      : 'POST',
-    url         : Urls.blEvent(eventName),
-    data        : JSON.stringify(eventArgs),
-    isAsync     : !!responder,
-    asyncHandler: responder
-  })
-}
+import { dispatchEvent } from './dispatch-event'
 
 const Events = {
 
-  dispatch: Utils.promisified(dispatch),
+  dispatch: Utils.promisified(dispatchEvent),
 
-  dispatchSync: Utils.synchronized(dispatch)
+  dispatchSync: Utils.synchronized(dispatchEvent)
 
 }
 
