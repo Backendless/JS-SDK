4d7b0afd55f0161e9b68c4914341d22631ba0341
Vladimir Upirov <valodya_upirov@mail.ru>
2018-04-02 18:36:37 +0300

    Bulk Operations in Data (#118)
    * - fix Bulk Operations
    - add e2e tests for Bulk Operations
    
    * - fix typo in TypeDefinition file

--- src/data/store/bulk.js
+++ src/data/store/bulk.js
@@ -1,76 +1,59 @@
 import Utils from '../../utils'
 import Urls from '../../urls'
 import Request from '../../request'
+import Async from '../../request/async'
 
-export function bulkCreate(objectsArray, asyncHandler) {
+export function bulkCreate(objects, asyncHandler) {
   const MSG_ERROR = (
-    'Invalid value for the "objectsArray" argument. ' +
-    'The argument must contain only array of objects.'
+    'Invalid bulkCreate argument. ' +
+    'The first argument must contain only array of objects.'
   )
 
-  if (!Utils.isArray(objectsArray)) {
+  if (!Utils.isArray(objects)) {
     throw new Error(MSG_ERROR)
   }
 
-  objectsArray.forEach(obj => {
-    if (!Utils.isObject(obj)) {
+  objects.forEach(obj => {
+    if (!Utils.isObject(obj) || Array.isArray(obj)) {
       throw new Error(MSG_ERROR)
     }
   })
 
   return Request.post({
     url         : Urls.dataBulkTable(this.className),
-    data        : objectsArray,
+    data        : objects,
     isAsync     : !!asyncHandler,
     asyncHandler: asyncHandler
   })
 }
 
-export function bulkUpdate(templateObject, where, asyncHandler) {
-  if (!templateObject || !Utils.isObject(templateObject)) {
-    throw new Error('Invalid templateObject argument. The first argument must contain object')
+export function bulkUpdate(where, changes, asyncHandler) {
+  if (!where || !Utils.isString(where)) {
+    throw new Error('Invalid bulkUpdate argument. The first argument must be "whereClause" string.')
   }
 
-  if (!where || !Utils.isString(where)) {
-    throw new Error('Invalid whereClause argument. The first argument must contain "whereClause" string.')
+  if (!Utils.isObject(changes) || Array.isArray(changes) || (changes instanceof Async)) {
+    throw new Error('Invalid bulkUpdate argument. The second argument must be object.')
   }
 
   return Request.put({
     url         : Urls.dataBulkTable(this.className),
     query       : { where },
-    data        : templateObject,
+    data        : changes,
     isAsync     : !!asyncHandler,
     asyncHandler: asyncHandler
   })
 }
 
-export function bulkDelete(objectsArray, asyncHandler) {
-  const MSG_ERROR = (
-    'Invalid bulkDelete argument. ' +
-    'The first argument must contain array of objects or array of id or "whereClause" string'
-  )
-
-  if (!Utils.isArray(objectsArray) && !Utils.isString(objectsArray)) {
-    throw new Error(MSG_ERROR)
+export function bulkDelete(where, asyncHandler) {
+  if (!Utils.isArray(where) && !Utils.isString(where)) {
+    throw new Error(
+      'Invalid bulkDelete argument. ' +
+      'The first argument must contain array of objects or array of id or "whereClause" string.'
+    )
   }
 
-  let where
-
-  if (Utils.isString(objectsArray)) {
-    where = objectsArray
-
-  } else if (Utils.isArray(objectsArray)) {
-
-    const objects = objectsArray.map(obj => {
-      if (!Utils.isObject(obj) && !Utils.isString(obj)) {
-        throw new Error(MSG_ERROR)
-      }
-
-      return `'${Utils.isString(obj) ? obj : obj.objectId}'`
-    })
-
-    where = `objectId in (${objects.join(',')})`
-  }
+  where = Utils.isString(where) ? where : objectsToWhereClause(where)
 
   return Request.delete({
     url         : Urls.dataBulkTable(this.className),
@@ -78,4 +61,19 @@ export function bulkDelete(objectsArray, asyncHandler) {
     isAsync     : !!asyncHandler,
     asyncHandler: asyncHandler
   })
+}
+
+function objectsToWhereClause(objects) {
+  const objectIds = objects.map(obj => {
+    if (!obj || (!Utils.isString(obj) && !obj.objectId)) {
+      throw new Error(
+        'Can not transform "objects" to "whereClause". ' +
+        'Item must be a string or an object with property "objectId" as string.'
+      )
+    }
+
+    return `'${Utils.isString(obj) ? obj : obj.objectId}'`
+  })
+
+  return `objectId in (${objectIds.join(',')})`
 }
