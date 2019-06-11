438413546bcc7b6154b5832cc63bab541a35a798
Stanislaw Grin <stanislaw.grin@gmail.com>
2019-06-11 17:39:36 +0300

    improve load relations query builder BKNDLSS-18026

--- backendless.d.ts
+++ backendless.d.ts
@@ -245,6 +245,16 @@ declare module __Backendless {
 
         setRelationName(relationName: string): Backendless.LoadRelationsQueryBuilder;
 
+        getProperties(): Array<string>;
+
+        setProperties(properties: string | Array<string>): Backendless.LoadRelationsQueryBuilder;
+
+        addProperty(properties: string): Backendless.LoadRelationsQueryBuilder;
+
+        getWhereClause(): string;
+
+        setWhereClause(whereClause: string): Backendless.LoadRelationsQueryBuilder;
+
         setPageSize(pageSize: number): Backendless.LoadRelationsQueryBuilder;
 
         setOffset(offset: number): Backendless.LoadRelationsQueryBuilder;
