28dee4dc875de3577408c3c9f2a77ae67328154c
Vladimir Upirov <vladimir.upirov@themidnightcoders.com>
2020-03-29 12:39:24 +0300

    - remove Backendless.debugMode from Logging Tests

--- test/e2e/specs/logging.js
+++ test/e2e/specs/logging.js
@@ -21,7 +21,6 @@ describe('Backendless.Logging', function() {
   })
 
   it('send messages pool', async () => {
-    Backendless.debugMode = true
     Backendless.Logging.setLogReportingPolicy(100, 100)
 
     const logger = Backendless.Logging.getLogger('MyLogger')
@@ -36,7 +35,5 @@ describe('Backendless.Logging', function() {
     logger.debug('I\'m debug message')
 
     await Backendless.Logging.flush()
-
-    Backendless.debugMode = false
   })
 })
