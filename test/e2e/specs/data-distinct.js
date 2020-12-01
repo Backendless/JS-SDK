356b686569a78452606d29cbb84392f5892e2d14
viktor.liablin <l9blin@gmail.com>
2020-12-01 23:31:03 +0200

    Js-sdk distinct tests

--- test/e2e/specs/data-distinct.js
+++ test/e2e/specs/data-distinct.js
@@ -3,32 +3,116 @@ import sandbox from '../helpers/sandbox'
 const Backendless = sandbox.Backendless
 
 describe('Data - Distinct', function() {
-  let parentTableStore
+  let personTableStore
 
   let queryBuilder
 
   sandbox.forSuite()
 
-  const PARENT_TABLE_NAME = 'Parent'
+  function sortArrayWithObjByPropsValue(a, b) {
+    if (a.name > b.name) {
+      return 1
+    }
+    if (a.name < b.name) {
+      return -1
+    }
+
+    return 0
+  }
+
+  const PERSON_TABLE_NAME = 'Person'
 
   before(async function() {
-    parentTableStore = Backendless.Data.of(PARENT_TABLE_NAME)
+    personTableStore = Backendless.Data.of(PERSON_TABLE_NAME)
 
-    const guy1 = await parentTableStore.save({ name: 'test1', age: 35, blocked: false, })
-    const guy2 = await parentTableStore.save({ name: 'test1', age: 35, blocked: false, })
-    const guy3 = await parentTableStore.save({ name: 'test3', age: 36, blocked: false, })
-    const guy4 = await parentTableStore.save({ name: 'test4', age: 37, blocked: false, })
-
+    await personTableStore.save({ name: 'name1', age: 10 })
+    await personTableStore.save({ name: 'name2', age: 20 })
+    await personTableStore.save({ name: 'name2', age: 20 })
   })
 
   beforeEach(async function() {
     queryBuilder = Backendless.DataQueryBuilder.create()
   })
 
-  it('load all properties by default', async () => {
-    const result = await parentTableStore.find(queryBuilder.setDistinct(true))
+  it('load', async () => {
+    let result = await personTableStore.find(queryBuilder.addProperty('name').setDistinct(true))
 
-    expect(Object.keys(result))
-      .to.eql([])
+    result = result.sort(sortArrayWithObjByPropsValue)
+
+    expect(result.sort(sortArrayWithObjByPropsValue)).to.eql(
+      [
+        {
+          "___class": "Person",
+          "name"    : "name1",
+        },
+        {
+          "___class": "Person",
+          "name"    : "name2",
+        }
+      ]
+    )
+  })
+
+  it('load1', async () => {
+    let result = await personTableStore.find(queryBuilder.addProperty('name').setDistinct(false))
+
+    result = result.sort(sortArrayWithObjByPropsValue)
+
+    expect(result).to.eql(
+      [
+        {
+          "___class": "Person",
+          "name"    : "name1",
+        },
+        {
+          "___class": "Person",
+          "name"    : "name2",
+        },
+        {
+          "___class": "Person",
+          "name"    : "name2",
+        }
+      ]
+    )
+  })
+
+  it('load2', async () => {
+    let result = await personTableStore.find(queryBuilder.addProperties('name', 'age').setDistinct(true))
+
+    result = result.sort(sortArrayWithObjByPropsValue)
+
+    expect(result).to.eql(
+      [
+        {
+          "___class": "Person",
+          "age"     : 10,
+          "name"    : "name1",
+        },
+        {
+          "___class": "Person",
+          "age"     : 20,
+          "name"    : "name2",
+        }
+      ]
+    )
+  })
+
+  it('load3', async () => {
+    let result = await personTableStore.find(queryBuilder.addProperty('name').setGroupBy('objectId').setDistinct(true))
+
+    result = result.sort(sortArrayWithObjByPropsValue)
+
+    expect(result).to.eql(
+      [
+        {
+          "___class": "Person",
+          "name"    : "name1",
+        },
+        {
+          "___class": "Person",
+          "name"    : "name2",
+        }
+      ]
+    )
   })
 })
