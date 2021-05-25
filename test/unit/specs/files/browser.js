0221b6973d6754150de65e83d2d121ac0a981d36
vladimir.upirov <valodya.upirov@gmail.com>
2021-05-26 02:16:25 +0300

    - improve method Files.upload to upload a file from URL

--- test/unit/specs/files/browser.js
+++ test/unit/specs/files/browser.js
@@ -192,6 +192,36 @@ describe('<Files> Browser', function() {
 
       await expect(Backendless.Files.upload(file1)).to.eventually.be.rejectedWith(errorMsg)
     })
+
+    it('uploads a file from URL', async () => {
+      const req1 = prepareMockRequest({ fileURL: 'target-file-url-1' })
+      const req2 = prepareMockRequest({ fileURL: 'target-file-url-2' })
+
+      const result1 = await Backendless.Files.upload('source-file-url-1', '/folder1/file1.jpg')
+      const result2 = await Backendless.Files.upload('source-file-url-2', '/folder2/sub/file2.txt', true)
+
+      expect(req1).to.deep.include({
+        method : 'POST',
+        path   : `${APP_PATH}/files/folder1/file1.jpg`,
+        headers: { 'Content-Type': 'application/json' },
+        body   : {
+          url: 'source-file-url-1',
+        }
+      })
+
+      expect(req2).to.deep.include({
+        method : 'POST',
+        path   : `${APP_PATH}/files/folder2/sub/file2.txt?overwrite=true`,
+        headers: { 'Content-Type': 'application/json' },
+        body   : {
+          url: 'source-file-url-2',
+        }
+      })
+
+      expect(result1).to.be.eql({ fileURL: 'target-file-url-1' })
+      expect(result2).to.be.eql({ fileURL: 'target-file-url-2' })
+    })
+
   })
 
 })
--- test/unit/specs/files/nodejs.js
+++ test/unit/specs/files/nodejs.js
@@ -34,6 +34,34 @@ describe('<Files> Nodejs', function() {
       expect(req1.body.getBoundary()).to.be.a('string')
     })
 
+    it('uploads a file from URL', async () => {
+      const req1 = prepareMockRequest({ fileURL: 'target-file-url-1' })
+      const req2 = prepareMockRequest({ fileURL: 'target-file-url-2' })
+
+      const result1 = await Backendless.Files.upload('source-file-url-1', '/folder1/file1.jpg')
+      const result2 = await Backendless.Files.upload('source-file-url-2', '/folder2/sub/file2.txt', true)
+
+      expect(req1).to.deep.include({
+        method : 'POST',
+        path   : `${APP_PATH}/files/folder1/file1.jpg`,
+        headers: { 'Content-Type': 'application/json' },
+        body   : {
+          url: 'source-file-url-1',
+        }
+      })
+
+      expect(req2).to.deep.include({
+        method : 'POST',
+        path   : `${APP_PATH}/files/folder2/sub/file2.txt?overwrite=true`,
+        headers: { 'Content-Type': 'application/json' },
+        body   : {
+          url: 'source-file-url-2',
+        }
+      })
+
+      expect(result1).to.be.eql({ fileURL: 'target-file-url-1' })
+      expect(result2).to.be.eql({ fileURL: 'target-file-url-2' })
+    })
   })
 
 })
