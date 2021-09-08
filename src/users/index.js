3b15ce5fa488e03b3f20eeabf12a6b6b0679e3e4
viktor.liablin <viktor.liablin@themidnightcoders.com>
2021-09-09 00:32:03 +0300

    BKNDLSS-25889 - Need codeless blocks for OAuth login

--- src/users/index.js
+++ src/users/index.js
@@ -307,10 +307,10 @@ export default class Users {
     })
   }
 
-  getAuthorizationUrlLink(providerCode, fieldsMapping, scope, redirect) {
+  getAuthorizationUrlLink(providerCode, fieldsMapping, scope, redirect, redirectAfterLoginUrl) {
     return this.app.request.post({
       url : this.app.urls.userAuthorizationURL(providerCode),
-      data: { fieldsMapping, permissions: scope, redirect }
+      data: { fieldsMapping, permissions: scope, redirect, redirectAfterLoginUrl }
     })
   }
 
