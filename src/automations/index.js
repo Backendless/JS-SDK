0ee3b2190dbaa5f3c5cda780b35f75d875231cfd
viktor.liablin <L9blin@gmail.com>
2024-05-27 22:00:14 +0300

    Automations. Support executionID for activateFlowTriggerById

--- src/automations/index.js
+++ src/automations/index.js
@@ -58,7 +58,7 @@ export default class Automations {
     })
   }
 
-  async activateFlowTriggerById(flowId, triggerId, data) {
+  async activateFlowTriggerById(flowId, triggerId, data, executionId) {
     if (!flowId || typeof flowId !== 'string') {
       throw new Error('The "flowId" argument must be provided and must be a string.')
     }
@@ -71,9 +71,20 @@ export default class Automations {
       throw new Error('The "data" argument must be an object.')
     }
 
+    if (executionId !== undefined && typeof executionId !== 'string') {
+      throw new Error('The "executionId" argument must be a string.')
+    }
+
+    const query = {}
+
+    if (executionId !== undefined) {
+      query.executionId = executionId
+    }
+
     return this.app.request.post({
-      url : `${this.app.urls.automationFlow()}/${flowId}/trigger/${triggerId}/activate`,
-      data: data || {},
+      url  : `${this.app.urls.automationFlow()}/${flowId}/trigger/${triggerId}/activate`,
+      data : data || {},
+      query: query,
     })
   }
 }
